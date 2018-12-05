1651c4641db51440d1c55af8a4ed1e58768e71ef
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-05 11:06:55 +0530

    table View Example

---
+++ TableViewWithNavExample/InfoTableViewController.swift
@@ -0,0 +1,116 @@
+//
+//  InfoTableViewController.swift
+//  TableViewWithNavExample
+//
+//  Created by Mac on 05/12/18.
+//  Copyright © 2018 tops. All rights reserved.
+//
+
+import UIKit
+
+class InfoTableViewController: UITableViewController {
+
+    let arrDict = [
+        ["Name":"Bhadresh","Course":"iOS"],
+        ["Name":"Reena","Course":"iOS"],
+        ["Name":"Kailash","Course":".Net"]
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return arrDict.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "infoCell", for: indexPath)
+
+        cell.textLabel?.text = arrDict[indexPath.row]["Name"]
+        cell.detailTextLabel?.text = arrDict[indexPath.row]["Course"]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        let detailsScene = self.storyboard?.instantiateViewController(withIdentifier: "detailsScene") as! ViewController
+        
+    
+        let item = arrDict[indexPath.row]
+        
+        detailsScene.item = item
+        
+        self.navigationController?.pushViewController(detailsScene, animated: true)
+        
+        
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TableViewWithNavExample/ViewController.swift
+++ TableViewWithNavExample/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  TableViewWithNavExample
+//  TableViewWithNavExample 
 //
 //  Created by Mac on 05/12/18.
 //  Copyright © 2018 tops. All rights reserved.
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var txtCourse: UITextField!
+    
+    var item = [String:String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        txtName.text = item["Name"]
+        txtCourse.text = item["Course"]
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .done, target: self, action: nil)
     }
 
     override func didReceiveMemoryWarning() {
